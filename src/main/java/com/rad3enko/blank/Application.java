dbfc52f94c75875bf70c443b0b14a282b6a71bbf
rad3enko <rad3enko@gmail.com>
2018-12-10 22:47:11 +1000

    init commit

---
+++ src/main/java/com/rad3enko/blank/Application.java
@@ -0,0 +1,13 @@
+package com.rad3enko.blank;
+
+import com.whitesoft.util.CustomRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        CustomRunner.run(Application.class, args, SpringApplication::run);
+    }
+}
